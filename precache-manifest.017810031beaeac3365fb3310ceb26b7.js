8c762fe80cc5798f52dfb8a450fb0a51a2985998
Michael Navarro <navarro.michael93@gmail.com>
2020-06-15 20:21:15 -0500

    Updates

---
+++ precache-manifest.017810031beaeac3365fb3310ceb26b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e0a8cbbb85d173f1146b8b24d164fbf",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "aaca260d7590ad42d565",
+    "url": "/react-todo/static/css/main.06f4b6d4.chunk.css"
+  },
+  {
+    "revision": "9f5f3d5860469e80ddaa",
+    "url": "/react-todo/static/js/2.1d5fa9b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.1d5fa9b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaca260d7590ad42d565",
+    "url": "/react-todo/static/js/main.adc54265.chunk.js"
+  },
+  {
+    "revision": "68172ead1f0e2993076f",
+    "url": "/react-todo/static/js/runtime-main.a53a9e8b.js"
+  }
+]);
